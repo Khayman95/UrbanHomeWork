fdfb5d56eb9b4ce7f90e7c26296705513ce82dd9
Khayman95 <hayman9695@gmail.com>
2024-10-15 15:40:49 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+Complited_homework = 12
+Spend_time = 1.5
+Course_name = 'Python'
+Time_for_one_task = Spend_time / Complited_homework
+print('Курс:', Course_name, 'Всего задач:', Complited_homework, 'Затрачено часов:', Spend_time, 'Среднее время выполнения:', Time_for_one_task)
