d0a91e40571261c0056962dc267c52174d73b612
Khayman95 <hayman9695@gmail.com>
2024-10-27 23:36:56 +0300

    Add files via upload

---
+++ module_2_14.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+b = 0
+a = len(numbers)
+c = 0
+while b <= a:
+    if b != a:
+        c = numbers[b]
+        b = b+1
+        is_prime = 0
+        for i in range(2, int(c ** 0.5) + 1):
+            if (c % i == 0):
+                is_prime = is_prime + 1
+        if is_prime <= 0:
+            primes.append(b)
+        else:
+            not_primes.append(b)
+    else:
+        break
+print("Простые",primes)
+print("Не простые",not_primes)
