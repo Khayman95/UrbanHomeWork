85a834a791416a6e21d6d1d368995b622d216fb5
Khayman95 <hayman9695@gmail.com>
2025-01-15 23:54:03 +0300

    Update module_9_12.py

--- module_9_12.py
+++ module_9_12.py
@@ -12,7 +12,6 @@ def get_advanced_writer(file_name):
         with open(file_name,'a', encoding='utf8') as file:
             for data in data_set:
                 file.write(str(data) + '\n')
-                file.close()
     return write_everything
 
 write = get_advanced_writer('example.txt')
